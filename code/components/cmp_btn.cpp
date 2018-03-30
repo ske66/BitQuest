e0b757af672cc62cb95be09b564be873af282151
DESKTOP-7VSRJDF\MarkB <40204953@live.napier.ac.uk>
2018-03-30 16:43:18 +0100

    added cmp_button

---
+++ code/components/cmp_btn.cpp
@@ -0,0 +1,53 @@
+#include "cmp_btn.h"
+#include "engine.h"
+#include <SFML/Window/Mouse.hpp>
+
+using namespace std;
+using namespace sf;
+
+BtnComponent::BtnComponent(Entity* p, shared_ptr<ShapeComponent> s, shared_ptr<TextComponent> t)
+	: _shapeCmp(s), _textCmp(t), Component(p)
+{
+	setHighlighted(false, true);
+}
+
+void BtnComponent::update(double dt)
+{
+	auto mousePos = Engine::GetWindow().mapPixelToCoords(Mouse::getPosition(Engine::GetWindow()));
+	if (_shapeCmp->getShape().getGlobalBounds().contains(mousePos))
+	{
+		setHighlighted(true);
+	}
+	else
+	{
+		setHighlighted(false);
+	}
+}
+
+void BtnComponent::setHighlighted(bool h, bool isClicked)
+{
+	if (h != _isHighlighted || isClicked)
+	{
+		_isHighlighted = h;
+		if (h)
+		{
+			_textCmp->getText().setFillColor(Color(240, 178, 0));
+		}
+		else
+		{
+			_textCmp->getText().setFillColor(Color(128, 128, 128));
+		}
+	}
+}
+
+bool BtnComponent::isSelected()
+{
+	if (_isHighlighted)
+	{
+		if (Mouse::isButtonPressed(Mouse::Left))
+		{
+			return true;
+		}
+	}
+	return false;
+}
---
+++ code/components/cmp_btn.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <ecm.h>
+#include "cmp_text.h"
+#include "cmp_animation.h"
+
+class BtnComponent : public Component
+{
+private:
+	bool _isHighlighted = false;
+	std::shared_ptr<ShapeComponent> _shapeCmp;
+	std::shared_ptr<TextComponent> _textCmp;
+
+public:
+	BtnComponent() = delete;
+	explicit BtnComponent(Entity* p, std::shared_ptr<ShapeComponent>, std::shared_ptr<TextComponent>);
+	void update(double dt) override;
+	void render() override {}
+	void setHighlighted(bool, bool isClicked = false);
+	bool isSelected();
+
+};
