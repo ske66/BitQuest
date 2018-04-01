d6b4a77aa16958c664282be693cc259e96f5d849
DESKTOP-7VSRJDF\MarkB <40204953@live.napier.ac.uk>
2018-04-01 10:59:44 +0100

    more optimisations made to tile engine

--- res/textures/tile_sheets/Level_Dungeon.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="Level_Dungeon" tilewidth="120" tileheight="120" tilecount="400" columns="20">
- <image source="Terrain.png" trans="ff00ff" width="2400" height="2400"/>
-</tileset>
